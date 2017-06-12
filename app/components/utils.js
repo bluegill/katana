cc0201ac0d518e7142c3c4acf40c16aef5c77502
bluegill <bluegill@users.noreply.github.com>
2017-06-12 16:29:22 -0400

    Update utils.js
    “Convert” utils module to a class instead of an object.

--- app/components/utils.js
+++ app/components/utils.js
@@ -1,21 +1,29 @@
-module.exports = {
-  getTimestamp: (unix) => {
+module.exports = class {
+  static getTimestamp (unix) {
     if (unix === true) {
       return (Math.floor(Date.now() / 1000))
     }
+
     const dateObj = new Date()
+
     let hours = dateObj.getHours()
+
     if (hours > 12) hours -= 12
-    else if (hours === 0) hours = 12
+    if (hours === 0) hours = 12
+
     let minutes = dateObj.getMinutes()
+
     if (minutes < 10) minutes = ('0' + minutes)
+
     const date = `${dateObj.getMonth() + 1}.${dateObj.getDate()}.${dateObj.getFullYear()}`
     const time = `${hours}:${minutes}:${dateObj.getSeconds()}`
-    return (`${date}_${time}`)
-  },
 
-  validateUrl: (url) => {
+    return (`${date}_${time}`)
+  }
+
+  static validateUrl (url) {
     const expression = /(ftp|http|https):\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
+
     return expression.test(url)
   }
 }
