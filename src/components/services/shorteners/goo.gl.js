4d54386409abef98a471b11c8c983bc7eec9d9a1
bluegill <gnood@hotmail.com>
2018-03-06 00:42:22 -0500

    Add license notice

--- src/components/services/shorteners/goo.gl.js
+++ src/components/services/shorteners/goo.gl.js
@@ -1,3 +1,22 @@
+/*
+ *  Katana - a powerful, open-source screenshot utility
+ *
+ *  Copyright (C) 2018, Gage Alexander <gage@washedout.co>
+ *
+ *  Katana is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  Katana  is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Katana. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const request = require('request')
 
 module.exports = class {
